7c16808a229cf503193a1d48f80d563bf6f36078
Kyle Harrington <czi@kyleharrington.com>
2024-02-16 13:48:49 -0500

    Adding new/updated ux_evaluation_winter2024_export_evaluation_0.0.1

---
+++ solutions/ux_evaluation_winter2024/export_evaluation/solution.py
@@ -0,0 +1,82 @@
+###album catalog: cellcanvas
+
+from album.runner.api import setup
+
+def run():
+    from album.runner.api import get_args
+    import appdirs
+    from pathlib import Path
+    import zipfile
+    import logging
+    import sys
+    import os
+
+    dataset_name = get_args().dataset_name
+    
+    all_datasets = ["cellcanvas_crop_007", "cellcanvas_crop_009", "cellcanvas_crop_010",]
+
+    if dataset_name not in all_datasets:
+        print(f"Dataset {dataset_name} invalid. Select from {all_datasets}")
+        return
+        
+    data_directory = Path(appdirs.user_data_dir("cellcanvas"))
+
+    zip_path = data_directory / f"{dataset_name}.zarr.zip"
+
+    zarr_directory = data_directory / f"{dataset_name}.zarr"
+
+    # Unzip the Zarr archive if the directory doesn't exist
+    if not zarr_directory.exists():
+        print(f"zarr {zarr_directory} doesn't exist. Failing.")
+        return
+
+    # Set up logging to file in the same directory as the zarr
+    log_file_path = zarr_directory / "cellcanvas.log"
+
+    home_directory = Path.home()
+    documents_directory = home_directory / "Documents"
+    documents_directory.mkdir(parents=True, exist_ok=True)
+    export_path = documents_directory / f"{dataset_name}_export.zip"
+            
+    with zipfile.ZipFile(export_path, 'w') as zipf:
+
+        for subdir in ["prediction", "painting"]:
+            subdir_path = zarr_directory / subdir
+            for root, dirs, files in os.walk(subdir_path):
+                for file in files:
+                    file_path = Path(root) / file
+                    zipf.write(file_path, arcname=file_path.relative_to(zarr_directory))
+        
+        # Add cellcanvas.log to the zip
+        zipf.write(log_file_path, arcname=log_file_path.relative_to(zarr_directory))
+
+    print(f"Exported results to {export_path}")
+
+        
+
+setup(
+    group="ux_evaluation_winter2024",
+    name="export_evaluation",
+    version="0.0.1",
+    title="Export results of the UX evaluation",
+    description="Export results of the UX evaluation.",
+    solution_creators=["Kyle Harrington"],
+    tags=["data conversion", "DataFrame", "Zarr", "Python"],
+    license="MIT",
+    covers=[{
+        "description": "Cover image.",
+        "source": "cover.png"
+    }],
+    album_api_version="0.5.1",
+    args=[
+        {"name": "dataset_name", "type": "string", "required": False, "description": "Path for the Zarr file", "default": "<all>"},
+    ],
+    run=run,
+    dependencies={
+        "parent": {
+            "group": "ux_evaluation_winter2024",
+            "name": "python-nographics",
+            "version": "0.0.2",
+        }
+    },
+)
