328af0c9cb2ad2bb84ebcde86dd77059cc9b3e8c
Kyle Harrington <czi@kyleharrington.com>
2024-04-10 17:06:51 -0400

    Adding new/updated cellcanvas_generate-pixel-embedding_0.0.18

--- solutions/cellcanvas/generate-pixel-embedding/solution.py
+++ solutions/cellcanvas/generate-pixel-embedding/solution.py
@@ -79,6 +79,60 @@ def run():
     data_path = get_data_path()
     model_path = os.path.join(data_path, "model_swinvit.pt")
 
+    def predict_zarr(zarr_path, output_directory, checkpoint_path, roi_size=(64, 64, 64), overlap=0.5, stitching_mode="gaussian"):
+        os.makedirs(output_directory, exist_ok=True)
+
+        # Load Zarr dataset
+        dataset = zarr.open(zarr_path, mode='r')
+        image = dataset[dataset.keys()[0]][:]
+
+        print(f"Processing image from {zarr_path} with shape {image.shape}")
+        
+        image = torch.from_numpy(np.expand_dims(image, axis=(0, 1)).astype(np.float32))
+        image = image.cuda()
+        
+        net = PixelEmbeddingSwinUNETR.load_from_checkpoint(checkpoint_path)
+
+        def predict_embedding(patch):
+            patch = (patch - patch.mean()) / patch.std()
+            return net(patch)
+
+        if torch.cuda.is_available():
+            image.cuda()
+        
+        with torch.no_grad():
+            result = sliding_window_inference(
+                inputs=image,
+                roi_size=roi_size,
+                sw_batch_size=1,
+                predictor=predict_embedding,
+                overlap=overlap,
+                mode=stitching_mode,
+                device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+            )
+
+        output_path = os.path.join(output_directory, zarr_file + '_embeddings.zarr')
+        
+        root = zarr.open(output_path, mode='w')
+        root.create_dataset(
+            "raw",
+            data=np.squeeze(image.cpu().numpy()),
+            chunks=(64, 64, 64),
+            compressor=zarr.Blosc(cname='zstd', clevel=3, shuffle=zarr.Blosc.SHUFFLE),
+            dtype=np.float32,
+            dimension_separator="/"
+        )
+        root.create_dataset(
+            "embedding",
+            data=np.squeeze(result.cpu().numpy()),
+            chunks=(1, 64, 64, 64),
+            compressor=zarr.Blosc(cname='zstd', clevel=3, shuffle=zarr.Blosc.SHUFFLE),
+            dtype=np.float32,
+            dimension_separator="/"
+        )
+
+        print(f"Prediction output saved to {output_path}")
+    
     def predict_mrc(input_file, output_directory, checkpoint_path, roi_size=(64, 64, 64), overlap=0.5, stitching_mode="gaussian"):
         os.makedirs(output_directory, exist_ok=True)
 
@@ -135,16 +189,19 @@ def run():
     output_directory = get_args().outputdirectory
     checkpoint_path = get_args().checkpointpath
 
-    predict_mrc(input_file, output_directory, checkpoint_path)
+    if input_file.lower().endswith('.mrc'):
+        predict_mrc(input_file, output_directory, checkpoint_path)
+    else:
+        predict_zarr(input_file, output_directory, checkpoint_path)
 
     print(f"Prediction output saved to {output_directory}")
 
 setup(
     group="cellcanvas",
     name="generate-pixel-embedding",
-    version="0.0.17",
+    version="0.0.18",
     title="Predict Tomogram Segmentations with SwinUNETR",
-    description="Apply a SwinUNETR model to MRC tomograms to produce embeddings, and save them in a Zarr.",
+    description="Apply a SwinUNETR model to a mrc or zarr tomogram to produce embeddings, and save them in a Zarr.",
     solution_creators=["Kyle Harrington"],
     cite=[{"text": "CellCanvas team.", "url": "https://cellcanvas.org"}],
     tags=["prediction", "deep learning", "cryoet", "tomogram"],
@@ -152,7 +209,7 @@ setup(
     covers=[],
     album_api_version="0.5.1",
     args=[
-        {"name": "inputfile", "type": "string", "required": True, "description": "Path to the input MRC file containing the tomogram"},
+        {"name": "inputfile", "type": "string", "required": True, "description": "Path to the input MRC file or zarr path containing the tomogram"},
         {"name": "outputdirectory", "type": "string", "required": True, "description": "Path for the output directory to save the H5 file"},
         {"name": "checkpointpath", "type": "string", "required": True, "description": "Path to the checkpoint file of the trained SwinUNETR model"},
     ],
