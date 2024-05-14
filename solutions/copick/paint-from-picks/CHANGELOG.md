7b170f299e8bd2eb3175302defea2dbed5beba81
Kyle Harrington <czi@kyleharrington.com>
2024-05-14 11:40:10 -0400

    Adding new/updated copick_paint-from-picks_0.1.5

--- solutions/copick/paint-from-picks/CHANGELOG.md
+++ solutions/copick/paint-from-picks/CHANGELOG.md
@@ -4,6 +4,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.5] - 2024-05-14
+Get user_id and session_id from pick_set
+
 ## [0.1.4] - 2024-05-14
 Track user_id and session_id
 
--- solutions/copick/paint-from-picks/solution.py
+++ solutions/copick/paint-from-picks/solution.py
@@ -141,9 +141,8 @@ def run():
         for pick_set in run.get_picks(obj.name):
             if pick_set and pick_set.points:
                 picks = [{'object_type': obj.name, 'location': (point.location.z, point.location.y, point.location.x)} for point in pick_set.points]
-                for point in pick_set.points:
-                    user_ids.add(point.user_id)
-                    session_ids.add(point.session_id)                    
+                user_ids.add(pick_set.user_id)
+                session_ids.add(pick_set.session_id)                    
                 paint_picks(run, painting_seg, picks, segmentation_mapping, voxel_spacing, ball_radius)
 
     print(f"Pickable objects: {[obj.name for obj in root.config.pickable_objects]}")
@@ -155,7 +154,7 @@ def run():
 setup(
     group="copick",
     name="paint-from-picks",
-    version="0.1.4",
+    version="0.1.5",
     title="Paint Copick Picks into a Segmentation Layer",
     description="A solution that paints picks from a Copick project into a segmentation layer in Zarr.",
     solution_creators=["Kyle Harrington"],
