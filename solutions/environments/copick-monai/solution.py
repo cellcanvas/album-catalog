6ce50e927c9f8cce123c218a92a763765d29fedd
Kyle Harrington <czi@kyleharrington.com>
2024-07-30 13:39:05 -0400

    Adding new/updated environments_copick-monai_0.0.1

---
+++ solutions/environments/copick-monai/solution.py
@@ -0,0 +1,54 @@
+###album catalog: cellcanvas
+
+from io import StringIO
+from album.runner.api import setup
+import tempfile
+import os
+
+env_file = """
+channels:
+  - pytorch
+  - nvidia
+  - conda-forge
+  - defaults
+dependencies:
+  - python==3.9
+  - pip
+  - pytorch
+  - torchvision
+  - torchaudio
+  - cudatoolkit
+  - pytorch-cuda
+  - dask
+  - einops
+  - h5py
+  - magicgui
+  - monai
+  - numpy<2
+  - pytorch-lightning
+  - qtpy
+  - rich
+  - scikit-image
+  - scipy
+  - tensorboard
+  - mrcfile
+  - pip:
+    - git+https://github.com/kephale/morphospaces.git@copick
+    - git+https://github.com/copick/copick.git
+    - git+https://github.com/kephale/copick-torch.git
+"""
+
+setup(
+    group="environments",
+    name="copick-monai",
+    version="0.0.1",
+    title="An environment to support copick monai projects",
+    description="An album solution for copick monai morphospaces projects .",
+    solution_creators=["Kyle Harrington"],
+    cite=[{"text": "Kyle Harrington", "url": "https://kyleharrington.com"}],
+    tags=["Python", "environment"],
+    license="MIT",
+    covers=[],
+    album_api_version="0.5.1",
+    dependencies={"environment_file": env_file},
+)
